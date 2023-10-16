cfde152c5b20df01897ef7ce3a7eb4396809c13e
Michael <michael.pecora.92@gmail.com>
2023-10-16 10:44:55 +0200

    Final

--- Template.js
+++ Template.js
@@ -61,19 +61,21 @@ console.log("La somma dei due numeri è: " + somma);
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
 
 /* ESERCIZIO 5
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name = "Michael"
 
 /* ESERCIZIO 6
- Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+ Esegui una sottrazione tra il numero 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let y = 4
+let sottrazione = y - x
+console.log("Il risultato della sottrazione è " + sottrazione);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -82,4 +84,8 @@ console.log("La somma dei due numeri è: " + somma);
  NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john"
+let name2 = "John"
+console.log(name1 !== name2); // name1 DIVERSO name2 => TRUE
+console.log(name1 === name2); // name1 UGUALE name2 => FALSE
+console.log(name1 === name2.toLowerCase()); // name1 UGUALE name2 SE name2 trasformato in minuscolo => TRUE
